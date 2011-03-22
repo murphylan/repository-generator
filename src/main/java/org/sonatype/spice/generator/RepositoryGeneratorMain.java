62d3f32cd52a9977bb7cd7881515d31fb394cc74
Marvin Froeder <velo@sonatype.com>
2011-03-22 14:47:55 +0000

    Revert r3045
    git-svn-id: file:///opt/svn/repositories/sonatype.org/spice/trunk/repository-generator@3046 5751e0cb-dcb7-432f-92e2-260806df54be

--- src/main/java/org/sonatype/spice/generator/RepositoryGeneratorMain.java
+++ src/main/java/org/sonatype/spice/generator/RepositoryGeneratorMain.java
@@ -6,28 +6,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Properties;
-import java.util.Random;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 
 public class RepositoryGeneratorMain
 {
-    public static final java.util.TimeZone TS_TZ = java.util.TimeZone.getTimeZone( "UTC" );
-
-    public static final java.text.DateFormat TS_FORMAT = new java.text.SimpleDateFormat( "yyyyMMdd.HHmmss" );
-
-    static
-    {
-        TS_FORMAT.setTimeZone( TS_TZ );
-    }
-
     private static final String LINE_ENDING = System.getProperty( "line.separator" );
-
+    
     private static Properties props = null;
 
     private static final String DEFAULT_GROUP_IDS = "group1,group2,group3";
@@ -64,10 +52,12 @@ public class RepositoryGeneratorMain
 
     private static final String PROPERTIES_FILE = "/repository-generator.properties";
 
-    private static final String PRE_POM = "<project" + LINE_ENDING + "    xmlns=\"http://maven.apache.org/POM/4.0.0\""
-        + LINE_ENDING + "    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" + LINE_ENDING
-        + "    xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">"
-        + LINE_ENDING + "    <modelVersion>4.0.0</modelVersion>" + LINE_ENDING;
+    private static final String PRE_POM =
+        "<project" + LINE_ENDING
+            + "    xmlns=\"http://maven.apache.org/POM/4.0.0\"" + LINE_ENDING
+            + "    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" + LINE_ENDING
+            + "    xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">" + LINE_ENDING
+            + "    <modelVersion>4.0.0</modelVersion>" + LINE_ENDING;
 
     private static final String POST_POM = "</project>" + LINE_ENDING;
 
@@ -174,63 +164,29 @@ public class RepositoryGeneratorMain
             String artifactId = parts[1];
             String version = parts[2];
 
-            String relativePath = groupId.replace( ".", "/" ) + "/" + artifactId + "/" + version + "-SNAPSHOT";
+            String relativePath = groupId.replace( ".", "/" ) + "/" + artifactId + "/" + version;
 
             File artifactDir = new File( baseOutputDir, relativePath );
             artifactDir.mkdirs();
 
-            int snapshots = (int) ( Math.random() * 10 );
-            for ( int j = 0; j < snapshots; j++ )
-            {
-                Random r = new Random();
-                Calendar cdr = Calendar.getInstance();
-                cdr.set( Calendar.HOUR_OF_DAY, j + 1 );
-                cdr.set( Calendar.MINUTE, r.nextInt( 59 ) );
-                cdr.set( Calendar.SECOND, r.nextInt( 59 ) );
-                long val1 = cdr.getTimeInMillis();
+            generateJar( artifactId, version, artifactDir );
+            generatePom( groupId, artifactId, version, artifactDir );
 
-                Date d = new Date( val1 );
-                String v = version + "-" + TS_FORMAT.format( d ) + "-" + ( j + 1 );
-
-                generateJar( groupId, artifactId, v, artifactDir, i, null );
-                generateJar( groupId, artifactId, v, artifactDir, i, "tests" );
-                generateJar( groupId, artifactId, v, artifactDir, i, "sources" );
-                generateJar( groupId, artifactId, v, artifactDir, i, "javadoc" );
-                generateJar( groupId, artifactId, v, artifactDir, i, "site" );
-                generatePom( groupId, artifactId, v, artifactDir );
-
-            }
             System.out.println( "Generated GAV #" + i + " " + gav );
             i++;
         }
     }
 
-    private static void generateJar( String groupId, String artifactId, String version, File dir, int i,
-                                     String classifier )
+    private static void generateJar( String artifactId, String version, File dir )
         throws IOException
     {
         JarOutputStream jos = null;
 
         try
         {
-            String c = classifier == null ? "" : "-" + classifier;
-            jos =
-                new JarOutputStream( new FileOutputStream( new File( dir, artifactId + "-" + version + c + ".jar" ) ) );
+            jos = new JarOutputStream( new FileOutputStream( new File( dir, artifactId + "-" + version + ".jar" ) ) );
             jos.putNextEntry( new JarEntry( "test/" ) );
             jos.closeEntry();
-
-            if ( classifier == null )
-            {
-                jos.putNextEntry( new JarEntry( "META-INF/maven/plugin.xml" ) );
-                String pluginXml =
-                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<plugin>\n" + "  <name>" + artifactId + "-"
-                        + version + "</name>\n" + "  <description>" + groupId + "-" + artifactId + "-" + version
-                        + "</description>\n" + "  <groupId>" + groupId + "</groupId>\n" + "  <artifactId>" + artifactId
-                        + "</artifactId>\n" + "  <version>" + version + "</version>\n" + "  <goalPrefix>" + artifactId
-                        + "</goalPrefix>\n" + "</plugin>\n";
-                jos.write( pluginXml.getBytes( "UTF-8" ) );
-                jos.closeEntry();
-            }
         }
         finally
         {
@@ -248,7 +204,6 @@ public class RepositoryGeneratorMain
         pom += "    <groupId>" + groupId + "</groupId>" + LINE_ENDING;
         pom += "    <artifactId>" + artifactId + "</artifactId>" + LINE_ENDING;
         pom += "    <version>" + version + "</version>" + LINE_ENDING;
-        pom += "    <packaging>maven-plugin</packaging>" + LINE_ENDING;
         pom += POST_POM;
 
         InputStream is = null;
